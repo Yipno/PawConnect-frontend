642e03f953017c728b42f5b06d1e7a5d17b8aef7
Yipno <142684910+Yipno@users.noreply.github.com>
2025-12-15 23:05:40 +0400

    -Home.js : ajout d'une fonction pour s'assurer que le style soit pris en compte en forcant un rerender au retour sur la page.

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,25 @@
+import React, { useCallback, useState } from 'react';
 import { Text, StyleSheet, View, Image } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Button from '../components/ui/Button';
 import useTheme from '../hooks/useTheme';
 
 export default function HomeScreen({ navigation }) {
   const { colors } = useTheme();
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  // Force le rechargement du composant à chaque fois qu’on y revient pour que le style s'affiche correctement
+  useFocusEffect(
+    //useFocusEffect permet d'exécuter un effet lorsque l'écran est focalisé
+    useCallback(() => {
+      //useCallback mémorise la fonction pour éviter de la recréer à chaque rendu
+      setRefreshKey(oldKey => oldKey + 1);
+    }, [])
+  );
+
   return (
-    <SafeAreaView className='flex-1 justify-evenly items-center bg-offwhite'>
+    <SafeAreaView key={refreshKey} className='flex-1 justify-evenly items-center bg-offwhite'>
       <View className=' w-11/12 items-center border-[1px] border-offwhite'>
         <Text className='text-h1 text-center text-deepSage font-manrope border-[1px] border-offwhite'>
           Bienvenue sur
